4bdc9804fb99fa003733793ea831747dc3dff36d
dariyaMakh <47285805+dariyaMakh@users.noreply.github.com>
2019-02-03 03:22:13 +0100

    bg color changed

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ class Settings():
         # Screen parameters
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 153, 153)
