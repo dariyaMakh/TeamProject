6fca783a355e58bbe5d4dfd58cf1505359aa4223
dariyaMakh <47285805+dariyaMakh@users.noreply.github.com>
2019-02-03 04:22:53 +0100

    bug fixed

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         # New ship appears in bottom
-        self.rect.center = self.screen_rect.center
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
     def blit(self):
