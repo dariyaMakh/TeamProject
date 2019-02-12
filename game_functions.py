1de4584bb103be36a913e84071319192ee3e3379
dariyaMakh <47285805+dariyaMakh@users.noreply.github.com>
2019-02-12 13:29:59 +0100

    add collision

--- game_functions.py
+++ game_functions.py
@@ -50,13 +50,14 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     # updates bullets position
     bullets.update()
     # Bullets are deleted when they reach the top border
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
